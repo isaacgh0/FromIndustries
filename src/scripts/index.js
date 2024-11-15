cb5855e487bbf9bb03f6afe5aef3d845da3ea1e6
isaacgh0 <garciaisaac468@gmail.com>
2024-11-15 00:19:17 -0600

    refactor: 📦 remove images in project
    remove project images and host on service
    sirv.com for web delivery, change some
    common styles and header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,31 +4,31 @@ const cards = [
   {
     header: 'Desarrollo de software',
     text:   'Desarrollamos aplicaciones web y soluciones generales de software a la medida, funcionales y que se ajusten a tu lógica de negocio.',
-    imgSrc: './src/assets/images/software-development.webp',
-    name:   'software'
+    imgSrc: 'https://fromindustries.sirv.com/Images/fi-landing-development.webp',
+    name:   'development'
   },
   {
     header: 'Administración de redes',
     text:   'Diseñamos, implementamos y administramos redes de computadoras, para que tu empresa cuente con una infraestructura de comunicación segura y eficiente.',
-    imgSrc: './src/assets/images/network-management.webp',
+    imgSrc: 'https://fromindustries.sirv.com/Images/fi-landing-network.webp',
     name:   'network'
   },
   {
     header: 'Asesoria y mantenimiento de hardware',
     text:   'Ofrecemos asesoría y mantenimiento de hardware, para que tu empresa cuente con equipos de cómputo en óptimas condiciones.',
-    imgSrc: './src/assets/images/hardware.webp',
+    imgSrc: 'https://fromindustries.sirv.com/Images/fi-landing-hardware.webp',
     name:   'hardware'
   },
   {
     header: 'Instalación de camaras de seguridad',
     text:   'Instalamos cámaras de seguridad y sistemas de monitoreo, para que tu empresa cuente con un sistema de vigilancia eficiente.',
-    imgSrc: './src/assets/images/camera-instalation.webp',
+    imgSrc: 'https://fromindustries.sirv.com/Images/fi-landing-camera.webp',
     name:   'camera'
   },
   {
     header: 'Soporte remoto',
     text:   'Ofrecemos soporte remoto para que tu empresa cuente con asistencia técnica inmediata y eficiente.',
-    imgSrc: './src/assets/images/product-support.webp',
+    imgSrc: 'https://fromindustries.sirv.com/Images/fi-landing-support.webp',
     name:   'support'
   }
 ];
